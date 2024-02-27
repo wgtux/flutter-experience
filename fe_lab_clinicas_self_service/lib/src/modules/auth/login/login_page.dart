be30b5eda4600c4ab3216c536913335e4353e651
Weslen Almeida <weslengomes@gmail.com>
2024-02-27 10:57:27 -0300

    feat(login): construindo a tela de login

--- fe_lab_clinicas_self_service/lib/src/modules/auth/login/login_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/auth/login/login_page.dart
@@ -6,9 +6,52 @@ class LoginPage extends StatelessWidget {
 
    @override
    Widget build(BuildContext context) {
+
+    final sizeOf = MediaQuery.sizeOf(context);
+
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
+           body: SingleChildScrollView(
+             child: Container(
+              constraints: BoxConstraints(minHeight: sizeOf.height),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background_login.png'),
+                  fit: BoxFit.cover
+                ),
+              ),
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(40),
+                  constraints: BoxConstraints( maxWidth: sizeOf.width * .8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16)
+                    ),
+                  child: Column(
+                    children: [
+                      const Text('Login'),
+                      SizedBox(height: 32),
+                      TextFormField(
+                        decoration: const InputDecoration(label: Text('Email')),
+                      ),
+                      SizedBox(height: 24),
+                      TextFormField(
+                        decoration: const InputDecoration(label: Text('password')),
+                      ),
+                      const SizedBox(height: 32),
+                      SizedBox(
+                        width: sizeOf.width * .8,
+                        height: 48,
+                        child: ElevatedButton(onPressed: () {}, 
+                          child: const Text('ENTRAR'),
+                        ),
+                      ),
+                    ],
+                    ),
+                ),
+              ),
+             ),
+           ),
        );
   }
 }
