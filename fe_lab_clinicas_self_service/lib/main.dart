4466d17ef5aebb9437df05c0268ed6b7394ee9a1
Weslen Almeida <weslengomes@gmail.com>
2024-03-29 14:10:07 -0300

    fix(login): Login e inicio da home page

--- fe_lab_clinicas_self_service/lib/main.dart
+++ fe_lab_clinicas_self_service/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:fe_lab_clinicas_self_service/src/binding/lab_clinicas_application_binding.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/auth/auth_module.dart';
@@ -6,7 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
 void main() {
-  runApp(const LabClinicasSelfServiceApp());
+  runZonedGuarded(() { 
+    runApp(const LabClinicasSelfServiceApp());
+  }, (error, stack) { 
+    log('Erro não tratado', error: error, stackTrace: stack);
+    throw error;
+  });
 }
 
 class LabClinicasSelfServiceApp extends StatelessWidget {
--- fe_lab_clinicas_self_service/lib/src/modules/auth/login/login_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/auth/login/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/auth/login/login_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:validatorless/validatorless.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,11 +13,23 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with MessageViewMixin {
 
   final formKey = GlobalKey<FormState>();
   final emailEC = TextEditingController();
   final passwordEC = TextEditingController();
+  final controller = Injector.get<LoginController>();
+
+  @override
+  void initState() {
+    messageListener(controller);
+    effect(() { 
+      if(controller.logged) {
+        Navigator.of(context).pushReplacementNamed('/home');
+      }
+    });
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -64,11 +79,24 @@ class _LoginPageState extends State<LoginPage> {
                           decoration: const InputDecoration(label: Text('Email')),
                         ),
                         SizedBox(height: 24),
-                        TextFormField(
-                          obscureText: true,
-                          controller: passwordEC,
-                          validator: Validatorless.required('Senha Obrigatória'),
-                          decoration: const InputDecoration(label: Text('password')),
+                        Watch((_) {
+                          return TextFormField(
+                            obscureText: controller.obscurePassword,
+                            controller: passwordEC,
+                            validator: Validatorless.required('Senha Obrigatória'),
+                            decoration: InputDecoration(
+                              label: const Text('Password'),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  controller.passwordToggle();
+                                }, 
+                                icon: controller.obscurePassword 
+                                ? const Icon(Icons.visibility)
+                                : const Icon(Icons.visibility_off),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                         const SizedBox(height: 32),
                         SizedBox(
@@ -78,7 +106,9 @@ class _LoginPageState extends State<LoginPage> {
                             onPressed: () {
                               final valid = formKey.currentState?.validate() ?? false;
 
-                              if(valid){}
+                              if(valid){
+                                controller.login(emailEC.text, passwordEC.text);
+                              }
                             }, 
                             child: const Text('ENTRAR'),
                           ),
